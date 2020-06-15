feb8551cda6832e6e1602b858d754c484b924be9
grace7937 <66059987+grace7937@users.noreply.github.com>
2020-06-15 23:37:04 +0900

    20200615_23:36

---
+++ greeting.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".js-form"),
+input = form.querySelector("input"),
+greeting = document.querySelector(".js-greetings");
+
+
+const USER_LS = "currentUser",
+SHOWWING
+
+function paintGreeting(text) {
+    greeting.classList.add()
+    greeting.innnerText = `Hello $(text)`
+
+}
+function loadName() {
+    const querySelector = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+
+    } else {
+        paintGreeting()
+    }
+}
+
+}
+
+
+
+
+init(){
+
+}
